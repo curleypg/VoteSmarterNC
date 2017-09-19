10ee21e8f12167df2a6d852d475d6a7cd30be964
Philip Curley <curleypg@gmail.com>
2017-09-18 20:06:30 -0400

    Improve data collection for individual member votes

--- ncleg/spiders/nc_leg_member_votes_spider.py
+++ ncleg/spiders/nc_leg_member_votes_spider.py
@@ -16,6 +16,7 @@ class NcLegMemberVotesSpider(scrapy.Spider):
         yield scrapy.Request(url=self.url.replace('%chamber%',self.chamber).replace('%session%', str(self.session)), callback=self.parse_members)
 
     def parse_members(self, response):
+        # Grab member data on their individual Roll Call votes
         for member in response.xpath('//div[@id="mainBody"]/ul/li'):
             info = MemberVotes()
             info['member'] = member.xpath('.//a/text()').extract_first().replace('\u00a0', ' ')
@@ -25,28 +26,38 @@ class NcLegMemberVotesSpider(scrapy.Spider):
             yield scrapy.Request(url=self.base+href, callback=self.parse_vote, meta={'item':info})
 
     def parse_vote(self, response):
+        # Grab the item from meta
         info = response.meta['item']
-        info['district'] = response.xpath('.//div[@id="title"]/text()')[0].re('\d+'),
-        for vote in response.xpath('//div[@id="mainBody"]/table/tr'):
-            motionData = vote.xpath('.//td[3]/text()').extract()
+        voteTable = response.xpath('//div[@id="mainBody"]/table/tr')
+        # Skip the first table row of header information
+        for vote in voteTable[1:]:
+            info['rcs'] = vote.xpath('td[1]/text()').extract_first()
+            info['district'] = response.xpath('//div[@id="title"]/text()').re_first('\d+')
+            # Get the bill motion
+            motionData = vote.xpath('td[3]/text()').extract()
             if len(motionData) > 1:
-                motion = motionData[0].strip() if len(motionData[0].strip()) > 1 else motionData[1].strip()
-                motionTwo = motionData[1].strip() if motionData[1].strip() != motion else ''
+                billTitle = motionData[0].strip() if len(motionData[0].strip()) > 1 else motionData[1].strip()
+                motion = motionData[1].strip() if motionData[1].strip() != billTitle else ''
             else:
-                motion = motionData[0].strip()
-                motionTwo = ''
+                billTitle = motionData[0].strip()
+                motion = ''
             info['chamber'] = 'House' if self.chamber == 'H' else 'Senate'
-            info['rcs'] = vote.xpath('.//td[1]/text()').extract_first()
-            info['bill'] = vote.xpath('.//td[2]/a/text()').extract_first()
+            # Check the bill ID
+            bill = vote.xpath('td[2]/a/text()').extract_first()
+            if bill.isspace():
+                info['bill'] = ''
+            else:
+                info['bill'] = vote.xpath('td[2]/a/text()').extract_first()
+            info['billTitle'] = billTitle
             info['motion'] = motion
-            info['motiontwo'] = motionTwo
-            info['date'] = vote.xpath('.//td[4]/text()').extract_first()
-            info['vote'] = vote.xpath('.//td[5]/text()').extract_first()
-            info['aye'] = vote.xpath('.//td[6]/text()').extract_first()
-            info['nay'] = vote.xpath('.//td[7]/text()').extract_first()
-            info['nv'] = vote.xpath('.//td[8]/text()').extract_first()
-            info['excabs'] = vote.xpath('.//td[9]/text()').extract_first()
-            info['excvote'] = vote.xpath('.//td[10]/text()').extract_first()
-            info['totalvote'] = vote.xpath('.//td[11]/text()').extract_first()
-            info['result'] = vote.xpath('.//td[12]/text()').extract_first()
+            info['date'] = vote.xpath('td[4]/text()').extract_first()
+            info['vote'] = vote.xpath('td[5]/text()').extract_first()
+            info['aye'] = vote.xpath('td[6]/text()').extract_first()
+            info['nay'] = vote.xpath('td[7]/text()').extract_first()
+            info['nv'] = vote.xpath('td[8]/text()').extract_first()
+            info['excabs'] = vote.xpath('td[9]/text()').extract_first()
+            info['excvote'] = vote.xpath('td[10]/text()').extract_first()
+            info['totalvote'] = vote.xpath('td[11]/text()').extract_first()
+            info['result'] = vote.xpath('td[12]/text()').extract_first()
+
             yield info
