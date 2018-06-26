156bcc9fc142d6dff3547176430b87ea9043d5d7
John D <john.demick@gmail.com>
2018-06-26 18:16:11 -0400

    refactored class and method names from test file

--- tests/utest.py
+++ tests/utest.py
@@ -1,13 +1,13 @@
 import unittest
-<<<<<<< HEAD
 import os
 import json
 os.system('scrapy crawl bills -a chamber=S -a number=1 -a session=2017 -o test.json')
 
-class TestStringMethods(unittest.TestCase):
-    def testFirstRecord(self):
+class TestBills(unittest.TestCase):
+    def testPrimarySponsorofSenateBill1(self):
         with open ('test.json') as f:
             data = json.load(f)
+            os.system('rm test.json')
         self.assertEqual(data[0]['primary_sponsors'][0].encode('ascii',errors='ignore'),'Rabon')
 
 if __name__ == '__main__':
