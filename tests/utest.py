3e3ff584832e2c981209ca9f10793d9fec9e8af5
John Demick <john.demick@gmail.com>
2018-06-26 17:39:43 -0400

    added test prototype

---
+++ tests/utest.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+import json
+os.system('scrapy crawl bills -a chamber=S -a number=1 -a session=2017 -o test.json')
+
+class TestStringMethods(unittest.TestCase):
+    def testFirstRecord(self):
+        with open ('test.json') as f:
+            data = json.load(f)
+        self.assertEqual(data[0]['primary_sponsors'][0].encode('ascii',errors='ignore'),'Rabon')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
