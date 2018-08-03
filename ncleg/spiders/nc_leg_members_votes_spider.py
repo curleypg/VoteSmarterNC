2598614c1015955d6541881a56fb179728b16274
Philip Curley <curleypg@gmail.com>
2018-08-02 22:06:29 -0400

    Update membersvotes spider

--- ncleg/spiders/nc_leg_members_votes_spider.py
+++ ncleg/spiders/nc_leg_members_votes_spider.py
@@ -6,13 +6,16 @@ class NcLegMemberVotesSpider(scrapy.Spider):
     name = "membersvotes"
     base = 'https://www.ncleg.net/'
     url = 'https://www.ncleg.net/gascripts/voteHistory/MemberVoteHistory.pl?sSession=%session%&sChamber=%chamber%'
+    urlMember = 'https://www.ncleg.net/gascripts/voteHistory/MemberVoteHistory.pl?sSession=%session%&sChamber=%chamber%&nUserID=%member%'
     # Set the available chambers (House and Senate)
     chambers = ['H','S']
 
-    def __init__(self, chamber='', session='',*args, **kwargs):
+    def __init__(self, bill='', chamber='', member='', session='',*args, **kwargs):
         super(NcLegMemberVotesSpider, self).__init__(*args, **kwargs)
-        self.session = session
+        self.bill = bill
         self.chamber = chamber
+        self.member = member
+        self.session = session
 
     def start_requests(self):
         # If a chamber is specified, scrape only it. Otherwise get both!
@@ -25,12 +28,16 @@ class NcLegMemberVotesSpider(scrapy.Spider):
     def parse_members(self, response):
         # Grab member data on their individual Roll Call votes
         for member in response.xpath('/html/body/div/table/tr/td/ul/li'):
+            session = self.session
             info = MemberVotes()
             info['member'] = member.xpath('.//a/text()').extract_first().replace('\u00a0', ' ')
             href = member.xpath('.//a/@href').extract_first()
             info['href'] = href
             info['memberId'] = parse_qs(urlparse(href).query)['nUserID'][0]
-            yield scrapy.Request(url=self.base+href, callback=self.parse_vote, meta={'item':info})
+            if len(self.member) == 0 or self.member == info['memberId']:
+                yield scrapy.Request(url=self.base+href, callback=self.parse_vote, meta={'item':info})
+            else:
+                continue
 
     def parse_vote(self, response):
         # Grab the item from meta
@@ -39,6 +46,9 @@ class NcLegMemberVotesSpider(scrapy.Spider):
         voteTable = response.xpath('/html/body/div/table/tr/td[1]/table/tr')
         # Skip the first table row of header information
         for vote in voteTable[1:]:
+            info['bill'] = vote.xpath('td[2]/a/text()').re_first('\d+')
+            if len(self.bill) > 0 and info['bill'] != self.bill:
+                continue
             info['rcs'] = vote.xpath('td[1]/text()').extract_first()
             info['district'] = response.xpath('//div[@id="title"]/text()').re_first('\d+')
             # Get the bill motion
@@ -51,11 +61,6 @@ class NcLegMemberVotesSpider(scrapy.Spider):
                 motion = ''
             info['chamber'] = 'House' if self.chamber == 'S' else 'Senate'
             # Check the bill ID
-            bill = vote.xpath('td[2]/a/text()').extract_first()
-            if bill.isspace():
-                info['bill'] = ''
-            else:
-                info['bill'] = vote.xpath('td[2]/a/text()').extract_first()
             info['billTitle'] = billTitle
             info['motion'] = motion
             info['date'] = vote.xpath('td[4]/text()').extract_first()
